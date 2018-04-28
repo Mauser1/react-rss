387347ee511fba4d610762fce775b44ea6063987
mauser1 <mauser91@gmail.com>
2018-04-28 17:43:05 +0200

    clarified actionTypes

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,28 +4,23 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 import firebase from 'firebase';
-import { signInSuccess, signOutSuccess, receiveFeedList } from '../actions';
+import { signInSuccess, receiveFeedList } from '../actions';
 import Appbar from '../components/Appbar';
-
 import SideBar from '../components/SideBar';
 import data from '../assets/data';
 
 class App extends Component {
   static propTypes = {
+    uid: PropTypes.string,
     children: PropTypes.element,
     signedIn: PropTypes.bool.isRequired,
-    uid: PropTypes.string.isRequired,
     signInSuccess: PropTypes.func.isRequired,
-    signOutSuccess: PropTypes.func.isRequired,
     receiveFeedList: PropTypes.func.isRequired,
   };
   componentDidMount() {
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
-      // is authed
       if (user) {
         this.props.signInSuccess(user);
-      } else {
-        this.props.signOutSuccess();
       }
       firebase.database().ref(`users/${this.props.uid}/feeds`).on('child_added', (values) => {
         this.props.receiveFeedList(values);
@@ -36,30 +31,22 @@ class App extends Component {
     this.removeListener();
   }
   render() {
-    const { signedIn } = this.props;
-    let menus;
-    if (signedIn) {
-      menus = data.signedIn;
-    } else {
-      menus = data.signedOut;
-    }
+    const menu = this.props.signedIn ? 'signedIn' : 'signedOut';
     return (
       <div>
         <Appbar />
-        <SideBar menus={menus} />
+        <SideBar menus={data[menu]} />
         <div>{this.props.children}</div>
       </div>
     );
   }
 }
-
 const mapStateToProps = state => ({
   signedIn: state.common.signedIn,
   uid: state.common.currentUser.uid,
 });
 const mapDispatchToProps = dispatch => ({
   signInSuccess: signInData => dispatch(signInSuccess(signInData)),
-  signOutSuccess: () => dispatch(signOutSuccess()),
   receiveFeedList: feedList => dispatch(receiveFeedList(feedList)),
 });
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
