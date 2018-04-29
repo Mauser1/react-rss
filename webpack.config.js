a48aec12bc7381fafe6b4be34817ff2609dd9b0c
mauser1 <mauser91@gmail.com>
2018-04-29 09:20:14 +0200

    Added wp config for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,26 @@
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: ['./src/index.js'],
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['eslint-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader', 'eslint-loader'],
+    },
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: { minimize: true },
+      }],
+    },
+    {
+      test: /\.css$/,
+      use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+    },
     ],
   },
   resolve: {
@@ -28,7 +31,13 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js',
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
   devServer: {
     contentBase: './dist',
     historyApiFallback: true,
